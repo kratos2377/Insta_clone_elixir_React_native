4bb166af4d3d8189ff2ff82cf7df629f19cbfb76
Shobhit Yadav <shobityadav23@gmail.com>
2021-06-12 22:28:04 -0700

    Initial Commit

---
+++ server/lib/server_web/router.ex
@@ -0,0 +1,36 @@
+defmodule ServerWeb.Router do
+  use ServerWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", ServerWeb do
+    pipe_through :api
+
+    forward "/graphql", Absinthe.Plug,
+    schema: InstagramWeb.Schema
+
+
+    if Mix.env == :dev do
+      forward "/graphiql", Absinth.Plug.GraphiQL,
+      schema: InstagramWeb.Schema
+    end
+  end
+
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+      live_dashboard "/dashboard", metrics: ServerWeb.Telemetry
+    end
+  end
+end
